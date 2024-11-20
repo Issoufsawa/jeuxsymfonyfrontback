6488832c7a1fff4518d04ec7822b801895864593
Issoufsawa <103884595+Issoufsawa@users.noreply.github.com>
2024-11-20 10:34:48 +0100

    rendez

--- src/Controller/AppointementadminController.php
+++ src/Controller/AppointementadminController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Appointement;
 use App\Repository\AppointementRepository; // Importe le repository de l'entité
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AppointementadminController extends AbstractController
 {
@@ -17,5 +20,27 @@ class AppointementadminController extends AbstractController
             'controller_name' => 'AppointementadminController',
             'alltest' => $alltest, 
         ]);
+    }
+
+
+    
+    #[Route('/appointement/{id}/delete', name: 'appointement_delete', methods: ['POST'])]
+    public function delete(Request $request, $id, EntityManagerInterface $entityManager): Response
+    {
+        // Utilisation de EntityManagerInterface pour récupérer l'entité Actuc
+        $actuc = $entityManager->getRepository(Appointement::class)->find($id);
+
+        if (!$actuc) {
+            throw $this->createNotFoundException('Le rendez-vous avec l\'ID ' . $id . ' n\'existe pas.');
+        }
+
+        // CSRF validation
+        if ($this->isCsrfTokenValid('delete' . $actuc->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($actuc);
+            $entityManager->flush();
+            $this->addFlash('success', 'rendez-vous supprimé avec succès!');
+        }
+
+        return $this->redirectToRoute('app_appointementadmin');
     }
 }
